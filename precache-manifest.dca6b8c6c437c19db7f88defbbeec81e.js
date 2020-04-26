a9ef9592d4150779d488181b72680369b3dadc31
Kranti Brid <kbrid@cainc.com>
2020-04-26 19:00:32 +0530

    Updates

---
+++ precache-manifest.dca6b8c6c437c19db7f88defbbeec81e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f120d18d6b37cf6aed685d774edecd",
+    "url": "/Marvel/index.html"
+  },
+  {
+    "revision": "a9b40a29c3d8a1e78892",
+    "url": "/Marvel/static/css/main.f5671102.chunk.css"
+  },
+  {
+    "revision": "ceef3734faf1c3ad9853",
+    "url": "/Marvel/static/js/2.eaba6bb8.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/Marvel/static/js/2.eaba6bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b40a29c3d8a1e78892",
+    "url": "/Marvel/static/js/main.c678e411.chunk.js"
+  },
+  {
+    "revision": "77c59da2fcea4a0fca18",
+    "url": "/Marvel/static/js/runtime-main.b56a8916.js"
+  },
+  {
+    "revision": "067285b0c7e98187bfeb0fe492712d2e",
+    "url": "/Marvel/static/media/Marvel.067285b0.ttf"
+  },
+  {
+    "revision": "99e95eaf9ab58d4d52ccfc65dc8dd1aa",
+    "url": "/Marvel/static/media/imageLoaded.99e95eaf.gif"
+  }
+]);
